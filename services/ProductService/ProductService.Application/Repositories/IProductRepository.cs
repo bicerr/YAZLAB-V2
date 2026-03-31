4636b5fc6bc8174cb2fc8474722f82b72e3684cb
bekir yılmaz <ebubekir.ylmzz@gmail.com>
2026-03-31 17:39:37 +0300

    GREEN: IProductRepository eklendi - testler başarılı

---
+++ services/ProductService/ProductService.Application/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+using ProductService.Domain.Entities;
+
+namespace ProductService.Application.Repositories;
+
+public interface IProductRepository
+{
+    Task<List<Product>> GetAllAsync();
+    Task<Product?> GetByIdAsync(string id);
+    Task CreateAsync(Product product);
+    Task<bool> UpdateAsync(string id, Product product);
+    Task<bool> DeleteAsync(string id);
+}
